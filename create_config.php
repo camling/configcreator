e37260f3b25d3b1099f2e69fccc680dab0e9f442
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-07-15 11:34:19 -0500

    commented out color_object tests

--- create_config.php
+++ create_config.php
@@ -6,21 +6,21 @@ $data = $_POST;
 // var_dump($data['colors_object']);
 // echo "</pre>";
 
-$data['colors_object'] = explode("|", $data['colors_object']);
-foreach($data['colors_object'] as $colors)
-{
-    $colors = json_decode($colors);
-    echo "<pre>";
-    var_dump($colors);
-    echo "</pre>";
+// $data['colors_object'] = explode("|", $data['colors_object']);
+// foreach($data['colors_object'] as $colors)
+// {
+//     $colors = json_decode($colors);
+//     echo "<pre>";
+//     var_dump($colors);
+//     echo "</pre>";
 
-}
+// }
 
 echo "<pre>";
 var_dump($data);
 echo "</pre>";
 
-$json_data = json_encode($data);
+$json_data = json_encode($data,JSON_UNESCAPED_SLASHES);
 
 echo "<pre>";
 var_dump($json_data);
