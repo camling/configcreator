d504dc75c410f105e797682926bdedb9a85dc419
MPPL <94387889+MPPL-DEV@users.noreply.github.com>
2022-07-15 14:32:09 -0500

    Create class structure for JSON output updated create_config.php Added ConfigObj class to create object from post Add Colors class to create a object from two arrays Class uses fliter_var to convert to bool from string Class uses empty_array_check function to check for empty array Class uses make_colors to combine body colors and text colors in object.

--- create_config.php
+++ create_config.php
@@ -2,29 +2,97 @@
 $data = $_POST;
 
 
+class Colors {
+    public $body;
+    public $text;
+
+    function __construct($body, $text)
+    {
+        $this->body = $body;
+        $this->text = $text;
+    }
+}
+
+class ConfigObj {
+
+    public $library_id;
+    public $name;
+    public $colors;
+    public $rotation_speed;
+    public $font;
+    public $logo;
+    public $alert;
+    public $show_qr_code;
+    public $event_type_ids;
+    public $start_date;
+    public $end_date;
+    public $locations;
+    public $age_groups;
+    public $is_ongoing;
+    public $only_featured_events;
+
+    function __construct($data)
+    {
+        $this->library_id = $data['library_id'];
+        $this->name = $data['name'];
+        $this->colors = $this->make_colors($data);
+        $this->rotation_speed = $data['rotation_speed'];
+        $this->font = $data['font'];
+        $this->logo = $data['logo'];
+        $this->alert = $data['alert'];
+        $this->show_qr_code = filter_var($data['show_qr_codes'], FILTER_VALIDATE_BOOLEAN);
+        $this->event_type_ids = $this->empty_array_check($data['event_type_ids']);
+        $this->start_date = $data['start_date'];
+        $this->end_date = $data['end_date'];
+        $this->locations = $this->empty_array_check($data['locations']);
+        $this->age_groups = $this->empty_array_check($data['age_type']);
+        $this->is_ongoing = filter_var($data['ongoing'], FILTER_VALIDATE_BOOLEAN);
+        $this->only_featured_events = filter_var($data['only_featured_events'], FILTER_VALIDATE_BOOLEAN);
+    }
+
+    protected function empty_array_check($array)
+    {
+        if(is_array($array))
+        {
+            if(!empty($array))
+            {
+                return $array;
+            }
+
+            return [];
+        }
+        else
+        {
+            return [];
+        }
+    }
+
+
+    protected function make_colors($data)
+    {
+        $colors_array = [];
+
+        for($i = 0; $i < count($data['body_colors']); $i++)
+        {
+            array_push($colors_array, new Colors($data['body_colors'][$i], $data['text_colors'][$i]));
+        }
+
+        return $colors_array;
+
+    }
+
+}
+
+$config_obj = new ConfigObj($data);
+
+
 // echo "<pre>";
-// var_dump($data['colors_object']);
+// var_dump($config_obj);
 // echo "</pre>";
 
-// $data['colors_object'] = explode("|", $data['colors_object']);
-// foreach($data['colors_object'] as $colors)
-// {
-//     $colors = json_decode($colors);
-//     echo "<pre>";
-//     var_dump($colors);
-//     echo "</pre>";
+$json_data = json_encode($config_obj,JSON_UNESCAPED_SLASHES);
 
-// }
-
-echo "<pre>";
-var_dump($data);
-echo "</pre>";
-
-$json_data = json_encode($data,JSON_UNESCAPED_SLASHES);
-
-echo "<pre>";
-var_dump($json_data);
-echo "</pre>";
+echo '<pre>' . print_r($json_data) . '</pre>';
 
 $bytes = file_put_contents("config_file.json", $json_data); 
 echo "The number of bytes written are $bytes.";
